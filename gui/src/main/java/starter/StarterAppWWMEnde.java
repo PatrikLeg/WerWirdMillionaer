38f61430df044f22aa21b405d235c041c04a38d6
Mikey <mikeybarbat@gmail.com>
2021-02-15 15:17:26 +0100

    Gui für das Quiz fertig mit Starterapp

---
+++ gui/src/main/java/starter/StarterAppWWMEnde.java
@@ -0,0 +1,31 @@
+package starter;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class StarterAppWWMEnde extends Application {
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL res = getClass().getResource("/WWM/WWMEnde.fxml");
+        Parent root = FXMLLoader.load(res);
+        primaryStage.setTitle("WWMEnde");
+        primaryStage.setScene(new Scene(root, 400, 300));
+
+        primaryStage.show();
+
+    }
+}
---
+++ gui/src/main/java/starter/StarterAppWWMStart.java
@@ -0,0 +1,31 @@
+package starter;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class StarterAppWWMStart extends Application {
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL res = getClass().getResource("/WWM/WWMStart.fxml");
+        Parent root = FXMLLoader.load(res);
+        primaryStage.setTitle("WWMStart");
+        primaryStage.setScene(new Scene(root, 400, 300));
+
+        primaryStage.show();
+
+    }
+}
