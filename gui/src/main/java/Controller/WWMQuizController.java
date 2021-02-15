e9448ad52464d3ad3a4ba60fa3a971853afd498d
Mikey <mikeybarbat@gmail.com>
2021-02-15 14:25:30 +0100

    Gui für das Quiz fertig

---
+++ gui/src/main/java/Controller/WWMQuizController.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+
+public class WWMQuizController {
+    public void Weiter(ActionEvent actionEvent) {
+    }
+
+    public void Beende(ActionEvent actionEvent) {
+    }
+}
