3d1906be2bc745a2ad50605000eecae15e33f6b7
PatrikLeg <PatrikLeggeri@web.de>
2021-02-16 15:50:51 +0100

    überarbeitet , Facts können nun angezeigt werden, random generator muss noch gemacht werden.

--- gui/src/main/java/de/azubi/wwm/datei/Funfacts.java
+++ gui/src/main/java/de/azubi/wwm/datei/Funfacts.java
@@ -2,66 +2,65 @@ package de.azubi.wwm.datei;
 
 public enum Funfacts {
     FF1(1,"Fügt man die Zeichen der Tesla-Modelle zusammen, entsteht daraus die Zeichenfolge ‘S3XY’, was für das Wort ‘Sexy’ steht."),
-    FF2(2,"Das Unternehmen Apple hat genügend Geld, um sich jedes beliebige Sportteam der Welt kaufen zu können.");
 
+    FF2(2,"Das Unternehmen Apple hat genügend Geld, um sich jedes beliebige Sportteam der Welt kaufen zu können."),
 
-/*
-    Funfacts ff3 = new Funfacts(3,"Es gibt mehr Leute, die das Logo von McDonald’s erkennen, als Leute, die das Kreuz des Christentums erkennen.");
+    FF3(3,"Es gibt mehr Leute, die das Logo von McDonald’s erkennen, als Leute, die das Kreuz des Christentums erkennen."),
 
-    Funfacts ff4 = new Funfacts(4,"Ein türkischer Vater hat seinem 19. Kind den Namen Yeter gegeben. Yeter bedeutet auf Deutsch ‘Es reicht’.");
+    FF4(4,"Ein türkischer Vater hat seinem 19. Kind den Namen Yeter gegeben. Yeter bedeutet auf Deutsch ‘Es reicht’."),
 
-    Funfacts ff5 = new Funfacts(5,"In der Weihnachtszeit nimmt eine erwachsene Person durchschnittlich 0,4 bis 0,9 Kilogramm zu.");
+    FF5(5,"In der Weihnachtszeit nimmt eine erwachsene Person durchschnittlich 0,4 bis 0,9 Kilogramm zu."),
 
-    Funfacts ff6 = new Funfacts(6,"1885 wurde Vodka in Russland ausschließlich in 12,3 Liter Eimern verkauft.");
+    FF6(6,"1885 wurde Vodka in Russland ausschließlich in 12,3 Liter Eimern verkauft."),
 
-    Funfacts ff7 = new Funfacts(7,"Erdbeeren werden nicht zu den Beeren, sondern zu den Nüssen gezählt.");
+    FF7(7,"Erdbeeren werden nicht zu den Beeren, sondern zu den Nüssen gezählt."),
 
-    Funfacts ff8 = new Funfacts(8,"Die früheren, amerikanischen Synchronsprecher von Mickey und Minnie Mouse waren verheiratet.");
+    FF8(8,"Die früheren, amerikanischen Synchronsprecher von Mickey und Minnie Mouse waren verheiratet."),
 
-    Funfacts ff9 = new Funfacts(9,"Auf dem Körper eines Menschen leben mehr Bakterien als Menschen auf der Erde.");
+    FF9(9,"Auf dem Körper eines Menschen leben mehr Bakterien als Menschen auf der Erde."),
 
-    Funfacts ff10 = new Funfacts(10,"Faultiere brauchen bis zu einem Monat, um ihr Essen zu verdauen.");
+    FF10(10,"Faultiere brauchen bis zu einem Monat, um ihr Essen zu verdauen."),
 
-    Funfacts ff11 = new Funfacts(11,"Jeder Mensch verliert täglich bis zu 200 Haare.");
+    FF11(11,"Jeder Mensch verliert täglich bis zu 200 Haare."),
 
-    Funfacts ff12 = new Funfacts(12,"Es gibt nur zwei Menschen, die das Coca-Cola Rezept kennen, sie dürfen deshalb nie zusammen reisen.");
+    FF12(12,"Es gibt nur zwei Menschen, die das Coca-Cola Rezept kennen, sie dürfen deshalb nie zusammen reisen."),
 
-    Funfacts ff13 = new Funfacts(13,"Das Durchschnittsalter der Soldaten in Vietnam war 19. Im Zweiten Weltkrieg lag es noch bei 26 Jahren.");
+    FF13(13,"Das Durchschnittsalter der Soldaten in Vietnam war 19. Im Zweiten Weltkrieg lag es noch bei 26 Jahren."),
 
-    Funfacts ff14 = new Funfacts(14,"Michael Jackson versuchte Marvel zu kaufen, weil er unbedingt die Rolle des Spiderman haben wollte.");
+    FF14(14,"Michael Jackson versuchte Marvel zu kaufen, weil er unbedingt die Rolle des Spiderman haben wollte."),
 
-    Funfacts ff15 = new Funfacts(15,"Korallen sind dem menschlichen Knochen so ähnlich, dass sie zur Behandlung von Knochenbrüchen eingesetzt werden.");
+    FF15(15,"Korallen sind dem menschlichen Knochen so ähnlich, dass sie zur Behandlung von Knochenbrüchen eingesetzt werden."),
 
-    Funfacts ff16 = new Funfacts(16,"Statistisch gesehen, werden die meisten Autos an Neujahr gestohlen.");
+    FF16(16,"Statistisch gesehen, werden die meisten Autos an Neujahr gestohlen."),
 
-    Funfacts ff17 = new Funfacts(17,"Es sterben jährlich mehr Menschen durch Sektkorken als durch den Biss einer giftigen Spinne.");
+    FF17(17,"Es sterben jährlich mehr Menschen durch Sektkorken als durch den Biss einer giftigen Spinne."),
 
-    Funfacts ff18 = new Funfacts(18,"Enten haben ebenso wie Menschen verschiedene Akzente.");
+    FF18(18,"Enten haben ebenso wie Menschen verschiedene Akzente."),
 
-    Funfacts ff19 = new Funfacts(19,"Ein Gramm DNA besitzt ungefähr genauso viele Informationen wie 600 Milliarden traditionelle CDs.");
+    FF19(19,"Ein Gramm DNA besitzt ungefähr genauso viele Informationen wie 600 Milliarden traditionelle CDs."),
 
-    Funfacts ff20 = new Funfacts(20,"Die kürzeste Flugstrecke dauert gerade einmal 47 Sekunden und bringt die Menschen in Schottland von der Insel Westray auf die Insel Papa Westray.");
+    FF20(20,"Die kürzeste Flugstrecke dauert gerade einmal 47 Sekunden und bringt die Menschen in Schottland von der Insel Westray auf die Insel Papa Westray."),
 
-    Funfacts ff21 = new Funfacts(21,"Das iPhone ist nach Schätzungen das profitabelste Produkt der Welt. Circa 50 Prozent des Preises sind der Gewinn für Apple.");
+    FF21(21,"Das iPhone ist nach Schätzungen das profitabelste Produkt der Welt. Circa 50 Prozent des Preises sind der Gewinn für Apple."),
 
-    Funfacts ff22 = new Funfacts(22,"Alkohol schützt vor radioaktiver Strahlung.");
+    FF22(22,"Alkohol schützt vor radioaktiver Strahlung."),
 
-    Funfacts ff23 = new Funfacts(23,"Die zwei häufigsten Gründe für schlechte Laune bei Menschen sind zu wenig Schlaf oder Hunger.");
+    FF23(23,"Die zwei häufigsten Gründe für schlechte Laune bei Menschen sind zu wenig Schlaf oder Hunger."),
 
-    Funfacts ff24 = new Funfacts(24,"In deutschen Universitäten werden Bücher mit juristischem Schwerpunkt am häufigsten gestohlen.");
+    FF24(24,"In deutschen Universitäten werden Bücher mit juristischem Schwerpunkt am häufigsten gestohlen."),
 
-    Funfacts ff25 = new Funfacts(25,"Lösegeldzahlungen an Entführer sind in Deutschland steuerlich absetzbar.");
+    FF25(25,"Lösegeldzahlungen an Entführer sind in Deutschland steuerlich absetzbar."),
 
-    Funfacts ff26 = new Funfacts(26,"Der durchschnittliche Preis für einen Liter schwarzer Druckerfarbe ist höher als der für einen Liter menschlichen Blutes.");
+    FF26(26,"Der durchschnittliche Preis für einen Liter schwarzer Druckerfarbe ist höher als der für einen Liter menschlichen Blutes."),
 
-    Funfacts ff27 = new Funfacts(27,"Die Zeit in der Kleopatra lebte, liegt näher zu dem Zeitpunkt der ersten Mondlandung als zum Bau der Pyramiden.");
+    FF27(27,"Die Zeit in der Kleopatra lebte, liegt näher zu dem Zeitpunkt der ersten Mondlandung als zum Bau der Pyramiden."),
 
-    Funfacts ff28 = new Funfacts(28,"In Schottland gibt es 421 verschiedene Begriffe für Schnee.");
+    FF28(28,"In Schottland gibt es 421 verschiedene Begriffe für Schnee."),
 
-    Funfacts ff29 = new Funfacts(29,"Der Panzer eines Gürteltiers ist kugelsicher.");
+    FF29(29,"Der Panzer eines Gürteltiers ist kugelsicher."),
 
-    Funfacts ff30 = new Funfacts(30,"Ein Blauwal nimmt mit einem Happs fast 500.000 Kilokalorien zu sich.");
-*/
+    FF30(30,"Ein Blauwal nimmt mit einem Happs fast 500.000 Kilokalorien zu sich.");
+
 
     public Integer getId() {
         return id;
@@ -88,7 +87,8 @@ public enum Funfacts {
     public Funfacts getZufallFact() {
         int anz = Funfacts.values().length;
         // TODO: Zufallszahl zwischen 1 und anz
-        int nr = 1;
+
+        int nr = 2;
         return Funfacts.valueOf("FF"+nr);
     }
 }
